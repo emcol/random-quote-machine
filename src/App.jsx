34297a5347f5bd643bfa17bf5f07598e943d2251
Emanuele Colabello <emanuele.colabello@gmail.com>
2023-11-15 16:31:03 +0100

    src/App.jsx: refactor App.jsx to display a quote with author and tweet button.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="quote-box">
+      <div className="quote-text">
+        <i className="fa fa-quote-left"></i>
+        <span id="text">Hello World!</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div id="author">- Author</div>
+      <button id="new-quote">New Quote</button>
+      <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank" rel="noreferrer">
+        Tweet Quote
+      </a>
+    </div>
+  );
 }
 
-export default App
+export default App;
