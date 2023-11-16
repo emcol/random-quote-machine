88e0298c9b603419e19b4b9aa3d71976e9363572
Emanuele Colabello <emanuele.colabello@gmail.com>
2023-11-16 10:12:55 +0100

    Add src/quotes.js.

---
+++ src/quotes.js
@@ -0,0 +1,14 @@
+const quotes = [
+  { text: "La vita è quello che ti succede mentre sei occupato a fare altri piani.", author: "John Lennon" },
+  { text: "Chi non ha mai commesso un errore non ha mai provato qualcosa di nuovo.", author: "Albert Einstein" },
+  { text: "Il nostro carattere è ciò che facciamo quando pensiamo che nessuno ci stia guardando.", author: "H. Jackson Brown Jr." },
+  { text: "Il successo è la somma di piccoli sforzi, ripetuti giorno dopo giorno.", author: "Robert Collier" },
+  { text: "Il coraggio non è l'assenza di paura, ma la capacità di andare avanti nonostante la paura.", author: "Mark Twain" },
+  { text: "La vita è come andare in bicicletta. Per mantenere l'equilibrio, devi muoverti.", author: "Albert Einstein" },
+  { text: "Non importa quanto sia difficile la vita, c'è sempre qualcosa che puoi fare e riuscire.", author: "Stephen Hawking" },
+  { text: "Il miglior momento per piantare un albero era 20 anni fa. Il secondo miglior momento è ora.", author: "Proverbio cinese" },
+  { text: "Non aspettare. Il tempo non sarà mai giusto.", author: "Napoleon Hill" },
+  { text: "Il tuo tempo è limitato, non sprecarlo vivendo la vita di qualcun altro.", author: "Steve Jobs" },
+];
+
+export default quotes;
