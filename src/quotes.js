545bae09e3c38f7b3aecb89668e8dcd500cf0d11
Emanuele Colabello <emanuele.colabello@gmail.com>
2023-11-16 17:36:56 +0100

    src/quotes.js: run prettier.

--- src/quotes.js
+++ src/quotes.js
@@ -1,14 +1,14 @@
 const quotes = [
-  { text: "La vita è quello che ti succede mentre sei occupato a fare altri piani.", author: "John Lennon", color: "#FF8A65" },
-  { text: "Chi non ha mai commesso un errore non ha mai provato qualcosa di nuovo.", author: "Albert Einstein", color: "#FFB900" },
-  { text: "Il nostro carattere è ciò che facciamo quando pensiamo che nessuno ci stia guardando.", author: "H. Jackson Brown Jr.", color: "#FFD54F" },
-  { text: "Il successo è la somma di piccoli sforzi, ripetuti giorno dopo giorno.", author: "Robert Collier", color: "#AED581" },
-  { text: "Il coraggio non è l'assenza di paura, ma la capacità di andare avanti nonostante la paura.", author: "Mark Twain", color: "#4DB6AC" },
-  { text: "La vita è come andare in bicicletta. Per mantenere l'equilibrio, devi muoverti.", author: "Albert Einstein", color: "#80DEEA" },
-  { text: "Non importa quanto sia difficile la vita, c'è sempre qualcosa che puoi fare e riuscire.", author: "Stephen Hawking", color: "#FF8A65" },
-  { text: "Il miglior momento per piantare un albero era 20 anni fa. Il secondo miglior momento è ora.", author: "Proverbio cinese", color: "#FFB900" },
-  { text: "Non aspettare. Il tempo non sarà mai giusto.", author: "Napoleon Hill", color: "#FFD54F" },
-  { text: "Il tuo tempo è limitato, non sprecarlo vivendo la vita di qualcun altro.", author: "Steve Jobs", color: "#AED581" },
+  { text: 'La vita è quello che ti succede mentre sei occupato a fare altri piani.', author: 'John Lennon', color: '#FF8A65' },
+  { text: 'Chi non ha mai commesso un errore non ha mai provato qualcosa di nuovo.', author: 'Albert Einstein', color: '#FFB900' },
+  { text: 'Il nostro carattere è ciò che facciamo quando pensiamo che nessuno ci stia guardando.', author: 'H. Jackson Brown Jr.', color: '#FFD54F' },
+  { text: 'Il successo è la somma di piccoli sforzi, ripetuti giorno dopo giorno.', author: 'Robert Collier', color: '#AED581' },
+  { text: "Il coraggio non è l'assenza di paura, ma la capacità di andare avanti nonostante la paura.", author: 'Mark Twain', color: '#4DB6AC' },
+  { text: "La vita è come andare in bicicletta. Per mantenere l'equilibrio, devi muoverti.", author: 'Albert Einstein', color: '#80DEEA' },
+  { text: "Non importa quanto sia difficile la vita, c'è sempre qualcosa che puoi fare e riuscire.", author: 'Stephen Hawking', color: '#FF8A65' },
+  { text: 'Il miglior momento per piantare un albero era 20 anni fa. Il secondo miglior momento è ora.', author: 'Proverbio cinese', color: '#FFB900' },
+  { text: 'Non aspettare. Il tempo non sarà mai giusto.', author: 'Napoleon Hill', color: '#FFD54F' },
+  { text: 'Il tuo tempo è limitato, non sprecarlo vivendo la vita di qualcun altro.', author: 'Steve Jobs', color: '#AED581' },
 ];
 
 export default quotes;
